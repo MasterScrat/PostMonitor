a148f97bf30d5ac9a540f04a13e32c0c7f5b97a6
flaurent <florian.laurent@gmail.com>
2016-05-07 15:26:15 +0200

    First working graph :D

--- server.py
+++ server.py
@@ -6,8 +6,12 @@ import itertools
 from flask import Flask, request, redirect, url_for
 from tinydb import TinyDB, where
 
-db = TinyDB('records.json')
+db = TinyDB('data/records.json')
 app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def root():
+	return app.send_static_file('www/index.html')
 
 @app.route('/all')
 def all():
@@ -16,21 +20,80 @@ def all():
 @app.route('/series')
 def series():
 	data = db.all()
-
-	# TODO shouldn't the DB do all that stuff for us?!
-	# use ElasticSearch instead!
 	data = sorted(data, key=lambda r: r['timestamp'])
 
 	# TODO should output series as 3 concatenated levels
 	grouped = {}
 	for record in data:
 		record_values = dict((key,value) for key, value in record.iteritems() if key in ('timestamp', 'score', 'num_comments'))
-		grouped.setdefault(to_unique_id(record), []).append(record_values)
+		grouped.setdefault(to_event_url(record), []).append(record_values)
 
-	return json.dumps(grouped, sort_keys=True, indent=4, separators=(',', ': '))
+	return print_json(grouped)
 
-def to_unique_id(record):
-		return record['project'] +'.'+ record['event'] +'.'+ record['url']
+@app.route('/values')
+def values():
+	# TODO shouldn't the DB do all that stuff for us?!
+	# use ElasticSearch instead!
+
+	data = db.all()
+	data = sorted(data, key=lambda r: r['timestamp'])
+
+	# first need ordered full lists of timestamps and URLs
+	# plus map of all values
+	all_timestamps = []
+	all_event_urls = []
+	event_url_score = {}
+
+	for record in data:
+		timestamp = record['timestamp']
+		if timestamp not in all_timestamps:
+			all_timestamps.append(timestamp)
+
+		event_url = to_event_url(record)
+		if event_url not in all_event_urls:
+			all_event_urls.append(event_url)
+
+		if event_url not in event_url_score:
+			event_url_score[event_url] = {timestamp: {}}
+
+		event_url_score[event_url][timestamp] = record['score']
+
+	# then for each timestamp, for each URL: check if there's a value
+	# if yes put it, if not put null
+	formatted_values = []
+
+	for timestamp in all_timestamps:
+		timestamp_values = [timestamp]
+
+		for event_url in all_event_urls:
+
+			if timestamp in event_url_score[event_url]:
+				timestamp_values.append(event_url_score[event_url][timestamp])
+			else:
+				timestamp_values.append(None)
+
+		formatted_values.append(timestamp_values)
+
+	return print_json(formatted_values)
+
+@app.route('/labels')
+def labels():
+	data = db.all()
+	data = sorted(data, key=lambda r: r['timestamp'])
+
+	all_event_urls = ['x']
+	for record in data:
+		event_url = to_event_url(record)
+		if event_url not in all_event_urls:
+			all_event_urls.append(event_url)
+
+	return print_json(all_event_urls)
+
+def to_event_url(record):
+		return record['project'] +' - '+ record['event'] +' - '+ record['url']
+
+def print_json(obj):
+	return json.dumps(obj, sort_keys=True, indent=4, separators=(',', ': '))
 
 @app.route('/clear')
 def clear():
